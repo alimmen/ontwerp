6979d78a1a6ca4921592685da78b2ece4014eec2
alimmen <adrianlimmen@gmail.com>
2016-01-14 17:14:21 -0500

    New Updates from my other computer
    Updates

--- script/main.js
+++ script/main.js
@@ -5,8 +5,15 @@ $(function() {
   // push button, submit form
   $("#pushbutton").click(function(e) {
   	e.preventDefault()
-  	    window.location.href = 'output.html';
-    return false;
+  	 //    window.location.href = 'output.html', '_blank';
+    // return false;
+    document.getElementById("title").style.visibility = "hidden";
+    document.getElementById("bucket").style.visibility = "hidden";
+    document.getElementById("bucket").style.height = "0px";
+    document.getElementById("start").classList.add("start");
+    document.getElementById("scrolltitle").classList.add("scrolltitle");
+    document.getElementById("scrolltext").classList.add("scrolltext");
+    document.getElementById("scroll").style.visibility = "visible";
     var starform1 = $(".starform1").val()
     var starform2 = $(".starform2").val()
     var starform3 = $(".starform3").val()
@@ -17,10 +24,8 @@ $(function() {
     var starform8 = $(".starform8").val()
     var starform9 = $(".starform9").val()
     var starform10 = $(".starform10").val()
-    console.log('project JS File');
-    console.log(starform3);
 
-    // form entry appears in HTML
+    // form entry appears in HTML (need it to appear in HTML)
     $(".magic1").empty().append(starform1);
     $(".magic2").empty().append(starform2);
     $(".magic3").empty().append(starform3);
@@ -36,6 +41,7 @@ $(function() {
   });
 
 });
+
 
 
 
