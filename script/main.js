c41b9c4ebcd1f8a403c5309ebb0eccd742b7911e
alimmen <adrianlimmen@gmail.com>
2016-01-14 20:55:55 -0500

    Forgot something
    Removed some things in JS

--- script/main.js
+++ script/main.js
@@ -23,8 +23,8 @@ $(function() {
     var starform6 = $(".starform6").val()
     var starform7 = $(".starform7").val()
     var starform8 = $(".starform8").val()
-    var starform9 = $(".starform9").val()
-    var starform10 = $(".starform10").val()
+    // var starform9 = $(".starform9").val()
+    // var starform10 = $(".starform10").val()
 
     // form entry appears in HTML (need it to appear in HTML)
     $(".magic1").empty().append(starform1);
@@ -35,8 +35,8 @@ $(function() {
     $(".magic6").empty().append(starform6);
     $(".magic7").empty().append(starform7);
     $(".magic8").empty().append(starform8);
-    $(".magic9").empty().append(starform9);
-    $(".magic10").empty().append(starform10);
+    // $(".magic9").empty().append(starform9);
+    // $(".magic10").empty().append(starform10);
 
 
   });
